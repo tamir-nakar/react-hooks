3545c239ae04ad66954a28a0c2fbf1fecd9a8afc
Tamir Nakar <nakar.tamir@gmail.com>
2022-03-11 23:10:47 +0200

    01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,11 +3,13 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName = ''}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
+
+    setName(event.target.value);
     // 🐨 update the name here based on event.target.value
   }
 
@@ -15,7 +17,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} value={name} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +25,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName='TAMIR' />
 }
 
 export default App
